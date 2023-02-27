11a1b148935d91c52e87547d34d054edfa423e09
NikolasTh90 <Nikolasth90@gmail.com>
2023-02-27 23:46:21 +0200

    asd

--- ProjectCode-2/n-body_std.c
+++ ProjectCode-2/n-body_std.c
@@ -662,21 +662,21 @@ int myMain(int argc, char *argv[], char* exec_type)
 int main(int argc, char *argv[])
 {
 	printf("Static\n");	
-	fflush(stdout)
+	fflush(stdout);
 	myMain(argc, argv, "static");
 
 	printf("Dynamic\n");
-	fflush(stdout)
+	fflush(stdout);
 	myMain(argc, argv, "dynamic");
 
 	
 	printf("Guided\n");
-	fflush(stdout)
+	fflush(stdout);
 	myMain(argc, argv, "guided");
 
 	
 	printf("Serial\n");
-	fflush(stdout)
+	fflush(stdout);
 	myMain(argc, argv, "serial");
 
 	return 0;
